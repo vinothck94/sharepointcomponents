e73870102c6c9f12f4ec2bafdc67de3c4ab3505f
ChandraMoorthy DakshinaMoorthy <chandramd@chennai.smartek21.st21>
2020-02-06 00:30:38 +0530

    Chandru first commit birthday and Annoversay webpart

---
+++ src/webparts/birthdaysAnniversaries/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IBirthdaysAnniversariesWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'BirthdaysAnniversariesWebPartStrings' {
+  const strings: IBirthdaysAnniversariesWebPartStrings;
+  export = strings;
+}
